5aa9989f6c599052cf875a0164bd0f9121a4e8a9
ftrotter-gov <frederick.trotter@cms.hhs.gov>
2026-03-24 22:26:33 -0400

    differentiate csv files

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python csv_to_json_is_simple.py src_csv/npi.csv cms-enrollment-in-good-standing apimock/cms-enrollment-in-good-standing.json
-python csv_to_json_is_simple.py src_csv/npi.csv cms-ial2-verified apimock/cms-ial2-verified.json
-python csv_to_json_is_simple.py src_csv/npi.csv aligned-with-cms-data-network apimock/aligned-with-cms-data-network.json
+python csv_to_json_is_simple.py src_csv/cms-enrollment-in-good-standing.csv cms-enrollment-in-good-standing apimock/cms-enrollment-in-good-standing.json
+python csv_to_json_is_simple.py src_csv/cms-ial2-verified.csv cms-ial2-verified apimock/cms-ial2-verified.json
+python csv_to_json_is_simple.py src_csv/aligned-with-cms-data-network.csv aligned-with-cms-data-network apimock/aligned-with-cms-data-network.json
